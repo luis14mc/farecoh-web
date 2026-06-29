c388e1467080ae0e73350789261793b185c8eb13
Luis Martinez <luismartinez.14mc@gmail.com>
2026-06-29 14:49:10 -0600

    feat: Astro7, bug fixes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,12 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-import node from "@astrojs/node";
+import vercel from "@astrojs/vercel";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   site: "https://farecoh.org",
   output: "server",
-  adapter: node({
-    mode: "standalone",
-  }),
+  adapter: vercel(),
   vite: {
     plugins: [tailwindcss()],
   },
